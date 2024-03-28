d233aeb876d19938913c35ecaf269994c88fb227
Sanda-Cyber <esterahirlet78@yahoo.es>
2024-03-28 02:09:26 -0400

    changes made from Sanda_cyber

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,18 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    return (
+<div className='alert alert-secondary' style={{ display: 'flex', justifyContent: 'center' }}>
+<label htmlFor="budget" style={{ marginRight: '10px' }}>Budget: £</label>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
 
